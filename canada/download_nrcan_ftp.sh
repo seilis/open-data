1a582b3e1d88be64861e71f9aed46433f8908da0
Aaron Seilis <aaron.seilis@seilis.ca>
2018-08-14 06:42:57 -0400

    Improve NRCAN download options
    - Add --continue to mirror download.
    - Reduce number of concurrent downloads to 4.

--- canada/download_nrcan_ftp.sh
+++ canada/download_nrcan_ftp.sh
@@ -9,4 +9,4 @@
 # This script uses lftp. Wikipedia: https://en.wikipedia.org/wiki/Lftp
 
 # Command to download everything in a "mirror" arrangement.
-lftp -e "mirror --parallel=10 --verbose ftp://ftp.geogratis.gc.ca/pub/nrcan_rncan gov-canada/ftp.geogratis.gc.ca/pub/nrcan_rncan"
+lftp -e "mirror --continue --parallel=4 --verbose ftp://ftp.geogratis.gc.ca/pub/nrcan_rncan gov-canada/ftp.geogratis.gc.ca/pub/nrcan_rncan"
